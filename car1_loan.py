2e3648e5297af58276bbe308c4c1b21f73ba37a8
saigangadharpappala <saigangadharpappala@gmail.com>
2025-07-09 23:17:08 +0530

    first python file

--- car1_loan.py
+++ car1_loan.py
@@ -1,3 +1,8 @@
-a = 9
-b = 10
-print(a+b)
+on_road = 1800000
+down_payment = 550000
+bank_interest = 10
+P = total_loan = on_road - down_payment
+N = loan_period_months = 18
+R = interest_permonth = (bank_interest/12)/100
+emi = (P*R*(1+R)**N) / ((1+R)**N-1)
+print(emi)
